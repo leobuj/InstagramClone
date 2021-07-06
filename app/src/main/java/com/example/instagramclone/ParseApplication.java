f830f44773863bab9df565e67f90d7609120a739
leobuj <l_b384@txstate.edi>
2021-07-06 15:51:27 -0500

    implemented login activity and layout implemented full login feature implemented ParseApplication in order to communicate with Parse user is persisted if after exiting the app if they remained logged in

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("8AaMvWnXxtzcsyi6IP1xM2jiQRy1PgCVRO391XdR")
+                .clientKey("ZLKf9Z3lTawcNwdNRtANsM1sNkCNFORkvjwDNLpq")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
+
